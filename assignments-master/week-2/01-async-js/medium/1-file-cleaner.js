6973d9f9af1e5b1163b2d8c4f048e292ed8a66b7
Triffycodes <adarsh.s.gowda20@gmail.com>
2024-05-24 19:43:32 -0500

    week 2 - medium

--- assignments-master/week-2/01-async-js/medium/1-file-cleaner.js
+++ assignments-master/week-2/01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,16 @@
 // ```
 // hello world my name is raman
 // ```
+
+let fs = require('fs');
+
+fs.readFile("/Users/adarsh/Desktop/projects/100xDev/assignments-master/week-2/01-async-js/medium/a.txt", "utf-8", function(err, data){
+    data=data.split(new RegExp("\\s+")).join(" ");
+    fs.writeFile("/Users/adarsh/Desktop/projects/100xDev/assignments-master/week-2/01-async-js/medium/a.txt", data, err=>{
+        if (err) {
+            console.error(err);
+          } else {
+            // file written successfully
+          }
+    });
+});
