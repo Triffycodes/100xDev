b5976f8646f4dfb12802260df5454c75943da373
Triffycodes <adarsh.s.gowda20@gmail.com>
2024-08-03 19:29:58 -0500

    Create jwt authentication
    Created jwt authetication for admin and users in course_selling_app.
    
    This is week03->04-mongo-with-jwt assignment

--- assignments-master/week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ assignments-master/week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,22 @@
+const jwt = require("jsonwebtoken");
+const {JWT_SECRET} = require("../config") //secret key for my jwtwebtoken
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const Token = req.headers.authorization;
+    const words = Token.split(" ");//words=["Bearer", "ascasvasncjnasjincans"] I need second part here
+    const jwtToken = words[1];
+
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+    if(decodedValue.username){
+        req.username = decodedValue.username;
+        next();
+    }else{
+        res.status(403).json({
+            msg: "You are not authenticated"
+        })
+    }
 }
 
 module.exports = userMiddleware;
